473f73bb87e8c1a7a1d7f824a65617254e31ba0f
Liz Bayardelle <lizbayardelle@gmail.com>
2016-05-20 11:09:38 -0700

    000.8 Cleaned/Precompiled Assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
-//= require bootsy
 //= require turbolinks
-//= require bootstrap
+//= require bootstrap-sprockets
+//= require bootsy
+//= require_tree .
